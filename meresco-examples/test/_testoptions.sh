a92a24135c50578fb9bb80a5acd5a3169ab5014b
wilkos <wilko.steinhoff@dans.knaw.nl>
2016-07-07 12:40:50 +0200

    Change location of integrationtest DB (works now).

--- meresco-examples/test/_testoptions.sh
+++ meresco-examples/test/_testoptions.sh
@@ -1,8 +1,4 @@
-# Set defaultencoding to UTF-8:
 export LANG=en_US.UTF-8
-
-# To be used with testing only! Checks proper use of yield/return, according Thijs:
 export WEIGHTLESS_COMPOSE_TEST="PYTHON"
-
-Sets the integrationtest database path for the integration tests (defaults to: /tmp): 
 export INTEGRATION_TEMPDIR_BASE="./integrationtest"
+# To be used with testing only! Checks proper use of yield/return.
