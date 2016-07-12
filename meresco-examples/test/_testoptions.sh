58264f1bc28bbb3df69de9414b0d3c82d00dd734
wilkos <wilko.steinhoff@dans.knaw.nl>
2016-07-12 11:11:31 +0200

    Create integrationtest dir in user dir.

--- meresco-examples/test/_testoptions.sh
+++ meresco-examples/test/_testoptions.sh
@@ -1,4 +1,5 @@
 export LANG=en_US.UTF-8
 export WEIGHTLESS_COMPOSE_TEST="PYTHON"
-export INTEGRATION_TEMPDIR_BASE="./integrationtest"
+export INTEGRATION_TEMPDIR_BASE="/home/wilkos/meresco/meresco-examples/test/integration_dir"
+export REMOTE_USERNAME="wilkos"
 # To be used with testing only! Checks proper use of yield/return.
