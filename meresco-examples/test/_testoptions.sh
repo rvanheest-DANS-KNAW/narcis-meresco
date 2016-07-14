735367234aadd49370c0012567701db378ddfa96
wilkos <wilko.steinhoff@dans.knaw.nl>
2016-07-14 09:33:40 +0200

    Changed integration test dir location.

--- meresco-examples/test/_testoptions.sh
+++ meresco-examples/test/_testoptions.sh
@@ -1,5 +1,5 @@
 export LANG=en_US.UTF-8
 export WEIGHTLESS_COMPOSE_TEST="PYTHON"
-export INTEGRATION_TEMPDIR_BASE="/home/wilkos/meresco/meresco-examples/test/integration_dir"
+export INTEGRATION_TEMPDIR_BASE="/home/wilkos/meresco/testintegration_dir"
 export REMOTE_USERNAME="wilkos"
 # To be used with testing only! Checks proper use of yield/return.
