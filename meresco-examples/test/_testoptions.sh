b5874597a26e1af16c085096a1c6c41516edb245
wilkos <wilko.steinhoff@dans.knaw.nl>
2016-09-15 16:09:39 +0200

    Renamed directories

--- meresco-examples/test/_testoptions.sh
+++
@@ -1,5 +0,0 @@
-export LANG=en_US.UTF-8
-export WEIGHTLESS_COMPOSE_TEST="PYTHON"
-export INTEGRATION_TEMPDIR_BASE="/home/wilkos/meresco/testintegration_dir"
-export REMOTE_USERNAME="wilkos"
-# To be used with testing only! Checks proper use of yield/return.
