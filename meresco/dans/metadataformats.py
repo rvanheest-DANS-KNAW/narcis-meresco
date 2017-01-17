5abc1258e9a077da813aede686309cb96317ed61
vesaa <vesa.akerman@dans.knaw.nl>
2017-01-17 14:17:05 +0100

    further...

--- meresco/dans/metadataformats.py
+++ meresco/dans/metadataformats.py
@@ -7,35 +7,11 @@
 from lxml.etree import _ElementTree
 from lxml import etree
 from meresco.components.xml_generic.validate import ValidateException
+from meresco.dans.namespacemapping import Namespaces
 
 
 class MetadataFormat():
     
-    NAMESPACEMAP = {
-        'oai_dc'    : 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-        'mods'      : 'http://www.loc.gov/mods/v3',
-        'datacite'  : 'http://datacite.org/schema/kernel-3'
-        'org'       : 'http://www.onderzoekinformatie.nl/nod/org',
-        'proj'      : 'http://www.onderzoekinformatie.nl/nod/act',
-        'prs'       : 'http://www.onderzoekinformatie.nl/nod/prs',
-        'didl'      : 'urn:mpeg:mpeg21:2002:02-DIDL-NS',
-        'dc'        : 'http://purl.org/dc/elements/1.1/',
-        'rdf'       : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-        'ore'       : 'http://www.openarchives.org/ore/terms/',
-    }
-    
-    NAMESPACEMAPPER = {
-        'oai_dc'       : NAMESPACEMAP.get('oai_dc'),
-        'didl_dc'      : NAMESPACEMAP.get('oai_dc'),
-        'didl_mods231' : NAMESPACEMAP.get('mods'),
-        'didl_mods30'  : NAMESPACEMAP.get('mods'),
-        'didl_mods36'  : NAMESPACEMAP.get('mods'),
-        'datacite'     : NAMESPACEMAP.get('datacite'),
-        'org'          : NAMESPACEMAP.get('org'),
-        'proj'         : NAMESPACEMAP.get('proj'),
-        'prs'          : NAMESPACEMAP.get('prs'),
-    }
-
     def __init__(self, lxmlNode, uploadId):
         
         md_format = None
@@ -67,7 +43,7 @@ class MetadataFormat():
             raise ValidateException("No known EduStandaard format was found in the metadata for uploadid: %s! This record cannot be processed." % (uploadId))
 
         self._format = md_format
-        self._namespace = NAMESPACEMAPPER.get(format, None)
+        self._namespace = Namespaces.NAMESPACEMAPPER.get(format, None)
          
 
     def getFormat(self):
---
+++ meresco/dans/namespacemapping.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+## begin license ##
+#
+#
+## end license ##
+
+
+class Namespaces():
+    
+    NAMESPACEMAP = {
+        'oai_dc'    : 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        'mods'      : 'http://www.loc.gov/mods/v3',
+        'datacite'  : 'http://datacite.org/schema/kernel-3',
+        'org'       : 'http://www.onderzoekinformatie.nl/nod/org',
+        'proj'      : 'http://www.onderzoekinformatie.nl/nod/act',
+        'prs'       : 'http://www.onderzoekinformatie.nl/nod/prs',
+        'didl'      : 'urn:mpeg:mpeg21:2002:02-DIDL-NS',
+        'dc'        : 'http://purl.org/dc/elements/1.1/',
+        'rdf'       : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'ore'       : 'http://www.openarchives.org/ore/terms/',
+    }
+    
+    NAMESPACEMAPPER = {
+        'oai_dc'       : NAMESPACEMAP.get('oai_dc'),
+        'didl_dc'      : NAMESPACEMAP.get('oai_dc'),
+        'didl_mods231' : NAMESPACEMAP.get('mods'),
+        'didl_mods30'  : NAMESPACEMAP.get('mods'),
+        'didl_mods36'  : NAMESPACEMAP.get('mods'),
+        'datacite'     : NAMESPACEMAP.get('datacite'),
+        'org'          : NAMESPACEMAP.get('org'),
+        'proj'         : NAMESPACEMAP.get('proj'),
+        'prs'          : NAMESPACEMAP.get('prs'),
+    }
