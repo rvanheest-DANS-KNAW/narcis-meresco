ae25b3104169a01541db8954c6dbe01bb41a997d
wilkos <wilko.steinhoff@dans.knaw.nl>
2017-01-26 12:38:16 +0100

    Moved API-server to port 8004

--- scripts/start_apiserver.sh
+++ scripts/start_apiserver.sh
@@ -22,7 +22,7 @@ fi
 
 LANG=en_US.UTF-8
 export LANG
-nohup sh -c "exec /home/meresco/meresco/narcisindex/bin/start-api --port=8003 --indexPort=8002 --gatewayPort=8000 --stateDir=/data/meresco/api 1>> ${LOG} 2>&1" >/dev/null &
+nohup sh -c "exec /home/meresco/meresco/narcisindex/bin/start-api --port=8004 --indexPort=8002 --gatewayPort=8000 --stateDir=/data/meresco/api 1>> ${LOG} 2>&1" >/dev/null &
 PID=$!
 echo $PID > $PIDFILE
 
