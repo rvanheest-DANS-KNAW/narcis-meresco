35a799f4f81fe0cc9a47162d75f95b80be89f040
wilkos <wilko.steinhoff@dans.knaw.nl>
2017-02-06 12:14:52 +0100

    Throw ValdidationError if WCP-collection is missing.

--- meresco/servers/index/normdoctofieldslist.py
+++ meresco/servers/index/normdoctofieldslist.py
@@ -36,6 +36,7 @@ from meresco.dans.longconverter import NormaliseOaiRecord
 
 from re import compile
 from meresco.dans.nameidentifier import Orcid, Dai, Isni, Rid, NameIdentifierFactory
+from meresco.components.xml_generic.validate import ValidateException
 
 
 namespacesmap = namespaces.copyUpdate({ #  See: https://github.com/seecr/meresco-xml/blob/master/meresco/xml/namespaces.py
@@ -176,7 +177,9 @@ class NormdocToFieldsList(Observable):
 
         # Get meta, header and metadata part(='long') from the normdoc:
         e_metapart = etree.fromstring(lxmlNode.xpath('/document:document/document:part[@name="meta"]/text()', namespaces=namespacesmap)[0])
-        wcp_collection = e_metapart.xpath('/meta:meta/meta:repository/meta:collection/text()', namespaces=namespacesmap)[0]
+        wcp_collection = e_metapart.xpath('/meta:meta/meta:repository/meta:collection/text()', namespaces=namespacesmap)
+        if not wcp_collection:
+            raise ValidateException("Collection is missing from metapart! Please add collection in WCP.")
          
         # print "lxml metapart:", tostring(e_metapart)
 
@@ -189,13 +192,13 @@ class NormdocToFieldsList(Observable):
             if self._verbose: print 'addField:', field.upper(), "-->", e_metapart.xpath(xpad, namespaces=namespacesmap)[0]
 
         record = None
-        if wcp_collection in WCPNODCOLLECTION:
+        if wcp_collection[0] in WCPNODCOLLECTION:
             record = e_recordpart.xpath('//prs:persoon | //prj:activiteit | //org:organisatie', namespaces=namespacesmap)
         else:
             record = e_recordpart.xpath('//norm:normalized/long:knaw_long', namespaces=namespacesmap)
         self._fillFieldslist(record[0], '')
 
-        self._addAuthorsAndNamesFields(record[0], wcp_collection)
+        self._addAuthorsAndNamesFields(record[0], wcp_collection[0])
         
         for field, xpad in fieldNamesXpathMap.iteritems():
             self._findAndAddToFieldslist(record[0], field, xpad)
