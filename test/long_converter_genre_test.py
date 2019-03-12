4282f9dd549d0065b47920b0ae02f374b7a6796b
Vesa Akerman <vesa.akerman@dans.knaw.nl>
2019-03-12 13:14:00 +0100

    Added Mods format in testing Genre

--- test/long_converter_genre_test.py
+++ test/long_converter_genre_test.py
@@ -31,43 +31,70 @@ from test.long_converter_test import LongConverterTest, baseXmls, long, namespac
 ELEMENT_VALUE = 'element_value'
 ATTRIBUTE_VALUE = 'attribute_value'
 EXPECTED = 'expected'
+MODS = 'mods'
 
 test_items = {Format.OAI_DC:   [{ELEMENT_VALUE: 'newspaper', EXPECTED: 'contributiontoperiodical'},
                                 {ELEMENT_VALUE: 'bookpart', EXPECTED: 'bookpart'}],
               Format.DATACITE: [{ELEMENT_VALUE: 'Dataset about software', ATTRIBUTE_VALUE: 'Software', EXPECTED: 'software'},
                                 {ELEMENT_VALUE: 'Dataset...', ATTRIBUTE_VALUE: '', EXPECTED: 'dataset'},
-                                {ELEMENT_VALUE: 'something', ATTRIBUTE_VALUE: 'just something', EXPECTED: 'dataset'}]
+                                {ELEMENT_VALUE: 'something', ATTRIBUTE_VALUE: 'just something', EXPECTED: 'dataset'},
+                                {ELEMENT_VALUE: 'Other collection', ATTRIBUTE_VALUE: '', EXPECTED: 'other collection'}],
+              MODS:             [{ELEMENT_VALUE: 'info:eu-repo/semantics/article', EXPECTED: 'article'},
+                                {ELEMENT_VALUE: 'info:eu-repo/semantics/something', EXPECTED: None}]
               }
+
 
 class LongConverterGenreTest(LongConverterTest):
 
     def test_genre(self):
+        print "\nRunning LongConverter Genre test",
         for format in Format:
             xml = etree.fromstring(baseXmls[format])
             self._test(format, xml)
+        print ""
 
-    def _test(self, format, xmlBase):
+    def _test(self, format, xml):
 
-        long._metadataformat = MetadataFormat(xmlBase, long._uploadid)
-        long._wcpcollection = 'dataset'
-        getattr(self, '_test_' + format.value)(format, xmlBase)
+        long._metadataformat = MetadataFormat(xml, long._uploadid)
+        if format == Format.OAI_DC:
+            self._test_oai_dc(format, xml)
+        elif format == Format.DATACITE:
+            self._test_datacite(format, xml)
+        elif long._metadataformat.isMods():
+            self._test_mods(MODS, xml)
 
     # OAI_DC
-    def _test_oai_dc(self, format, xmlBase):
+    def _test_oai_dc(self, format, xml):
 
         dc = "http://purl.org/dc/elements/1.1/"
         type = '{' + dc + '}' + 'type'
 
         for t in test_items[format]:
-            self._reset(xmlBase)
+            self._reset(xml)
             self._addElement(self.xml, type, t[ELEMENT_VALUE])
             self._assert(Item.GENRE, t[EXPECTED])
 
-    # DATACITE
-    def _test_datacite(self, format, xmlBase):
+    # MODS
+    def _test_mods(self, format, xml):
+
+        mods = "http://www.loc.gov/mods/v3"
+        genre = '{' + mods + '}' + 'genre'
 
         for t in test_items[format]:
-            self._reset(xmlBase)
-            resource = self.xml.find("datacite:resource", namespaces=namespacesmap)
+            self._reset(xml)
+            mods = self.xml.find(".//mods:mods", namespaces=namespacesmap)
+            self._addElement(mods, genre, t[ELEMENT_VALUE])
+            self._assert(Item.GENRE, t[EXPECTED])
+
+    # DATACITE
+    def _test_datacite(self, format, xml):
+
+        for t in test_items[format]:
+            if t[ELEMENT_VALUE] == 'Other collection':
+                long._wcpcollection = ''
+            else:
+                long._wcpcollection = 'dataset'
+            self._reset(xml)
+            resource = self.xml.find(".//datacite:resource", namespaces=namespacesmap)
             self._addElement(resource, 'resourceType', t[ELEMENT_VALUE], 'resourceTypeGeneral', t[ATTRIBUTE_VALUE])
             self._assert(Item.GENRE, t[EXPECTED])
